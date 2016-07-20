6daed25644341fc4159b0c093ed32af7f689f475
Martin Popel <popel@ufal.mff.cuni.cz>
2016-07-21 01:05:43 +0200

    Write::HTML has a button for downloading SVG

--- lib/Udapi/Block/Write/HTML.pm
+++ lib/Udapi/Block/Write/HTML.pm
@@ -6,20 +6,25 @@ has_ro path_to_js => (default=>'web');
 
 sub process_document {
     my ($self, $doc) = @_;
-    my ($jquery, $js_t_v);
+    my ($jquery, $js_t_v, $fsaver);
     if ($self->path_to_js eq 'web'){
         $jquery = 'https://code.jquery.com/jquery-2.1.4.min.js';
-        $js_t_v = 'http://ufal.github.io/js-treex-view/js-treex-view.js';
+        $fsaver = 'https://cdn.rawgit.com/eligrey/FileSaver.js/master/FileSaver.min.js';
+        $js_t_v = 'https://cdn.rawgit.com/ufal/js-treex-view/gh-pages/js-treex-view.js';
     } else {
         $jquery = $self->path_to_js . '/jquery-2.1.4.min.js';
+        $fsaver = $self->path_to_js . '/FileSaver.min.js';
         $js_t_v = $self->path_to_js . '/js-treex-view.js';
     }
     say '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">';
     say '<title>Udapi viewer</title>'; # TODO $doc->loaded_from
     say qq'<script src="$jquery"></script>';
+    say qq'<script src="$fsaver"></script>';
     say qq'<script src="$js_t_v"></script>';
-    say "</head>\n<body>\n<div id='treex-view'></div><script>";
-    print "data=[\n";
+    say "</head>\n<body>";
+    say '<button style="float:right" type="submit" onclick="saveTree()">'
+        .'<span>Save as SVG</span></button><div id="treex-view"></div><script>';
+    say 'data=[';
     foreach my $bundle ( $doc->bundles() ) {
         next if !$self->_should_process_bundle($bundle);
         print ',' if $bundle->number != 1;
@@ -51,7 +56,15 @@ sub process_document {
         say qq'},"desc":[$desc]}';
     }
     say '];';
-    say "\$('#treex-view').treexView(data);\n</script></body></html>";
+    say "\$('#treex-view').treexView(data);";
+    say q|function saveTree() {
+     var svg_el = jQuery('svg');
+     if (svg_el.length) {
+        var svg = new Blob([svg_el.parent().html()], {type: "image/svg+xml"});
+        saveAs(svg, 'tree.svg');
+     }
+    }|;
+    say '</script></body></html>';
     return;
 }
 
@@ -113,9 +126,10 @@ Udapi::Block::Write::HTML - HTML+JavaScript+SVG visualization of dependency tree
  udapi.pl Write::HTML < file.conllu > file.html
  firefox file.html
 
- # for offline use, we need to download first two JavaScript libraries
- wget http://ufal.github.io/js-treex-view/js-treex-view.js
+ # for offline use, we need to download first three JavaScript libraries
  wget https://code.jquery.com/jquery-2.1.4.min.js
+ wget https://cdn.rawgit.com/eligrey/FileSaver.js/master/FileSaver.min.js
+ wget https://cdn.rawgit.com/ufal/js-treex-view/gh-pages/js-treex-view.js
  udapi.pl Write::HTML path_to_js=. < file.conllu > file.html
  firefox file.html
 
@@ -127,9 +141,9 @@ Under each node its form, upos and deprel are shown.
 In the tooltip its lemma and (morphological) features are shown.
 After clicking the node, all other attributes are shown.
 When hovering over a node, the respective word in the (plain text) sentence
-is highlighted.
+is highlighted. There is a button for downloading trees as SVG files.
 
-Two JavaScript libraries are required (jquery and js-treex-view).
+Three JavaScript libraries are required (jquery, FileSaver and js-treex-view).
 By default they are linked online (so Internet access is needed when viewing),
 but they can be also downloaded locally (so offline browsing is possible and
 the loading is faster): see the Synopsis above.
@@ -147,14 +161,14 @@ so it can be viewed with any browser or even published online.
 
 When viewing the html file, the JavaScript library C<js-treex-view>
 generates an svg on the fly from the json.
-You can save the svg to file, e.g. using Chrome's Inspect (Ctrl+Shift+I).
 
 =head1 PARAMETERS
 
 =head2 path_to_js
 
 Path to jquery and js-treex-view.
-C<web> means http://ufal.github.io/js-treex-view/js-treex-view.js
+C<web> means http://ufal.github.io/js-treex-view/js-treex-view.js,
+https://cdn.rawgit.com/eligrey/FileSaver.js/master/FileSaver.min.js
 and https://code.jquery.com/jquery-2.1.4.min.js will be linked.
 When you use C<path_to_js=.> the libraries will be searched in the current directory.
 
